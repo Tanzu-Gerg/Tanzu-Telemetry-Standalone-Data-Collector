36aa463e73ef3061903a80b553b0edcf76a06e5d
Greg Cobb <gcobb@vmware.com>
2023-02-08 11:22:06 -0800

    Output results at JSON
    - Extract private methods
    - Fix bug if there is only a single page of apps returned

--- main.py
+++ main.py
@@ -3,15 +3,22 @@
 import json
 import subprocess
 
-from pprint import pprint
 from types import SimpleNamespace
 
 class AppLifecycle(SimpleNamespace):
     type: str
     buildpacks: list[str]
 
+    def as_dict(self) -> dict:
+       return self.__dict__
+
+
 class Droplet(SimpleNamespace):
     buildpacks: list[dict[str, str]]
+
+    def as_dict(self) -> dict:
+       return self.__dict__
+
 
 class App(SimpleNamespace):
     guid: str
@@ -20,7 +27,17 @@ class App(SimpleNamespace):
     current_droplet: Droplet | None = None
     env: dict
 
-PAGE_SIZE=5
+    def as_dict(self) -> dict:
+        return {
+                "guid": self.guid,
+                "state": self.state,
+                "lifecycle": self.lifecycle.as_dict(),
+                "current_droplet": self.current_droplet.as_dict(),
+                "env": self.env,
+                }
+
+
+PAGE_SIZE=5000
 
 def main():
     print("""
@@ -29,11 +46,19 @@ def main():
         logged in as an admin or admin_read_only user.
 ====================================================================
 """)
+    all_apps = _fetch_apps()
+    all_apps = _fetch_droplets(all_apps)
+    all_apps = _fetch_env(all_apps)
 
+    print("Apps:")
+    print(json.dumps([ app.as_dict() for app in all_apps ]))
+
+
+def _fetch_apps() -> list[App]:
     print("Fetching all apps...")
 
     current_app_page = 1
-    total_app_pages = "?"
+    total_app_pages = "1"
     all_apps = []
     while True:
         print(f"Fetching apps page {current_app_page}/{total_app_pages}", end="\r")
@@ -47,9 +72,6 @@ def main():
         apps_pagination = parsed_apps_response.get("pagination", {})
         total_app_pages = apps_pagination.get("total_pages")
 
-        if not apps_pagination.get("next"):
-            break
-
         apps = parsed_apps_response.get("resources")
         parsed_apps = [
             App(
@@ -62,8 +84,23 @@ def main():
         all_apps = all_apps + parsed_apps
 
         current_app_page += 1
-    print("\n")
 
+        if not apps_pagination.get("next"):
+            break
+
+    print("\n")
+    return all_apps
+
+
+def _construct_lifecycle(app: dict) -> AppLifecycle:
+    lifecycle = app.get("lifecycle", {})
+    return AppLifecycle(
+            type=lifecycle.get("type"),
+            buildpacks=lifecycle.get("data", {}).get("buildpacks") # TODO: Docker apps
+            )
+
+
+def _fetch_droplets(all_apps: list[App]) -> list[App]:
     print("Fetching droplets...")
     for index, app in enumerate(all_apps):
         print(f"Fetching droplet {index + 1}/{len(all_apps)}", end="\r")
@@ -75,7 +112,10 @@ def main():
         parsed_droplet_response = json.loads(droplet_response_raw) # TODO: 404 case
         app.current_droplet = Droplet(buildpacks=parsed_droplet_response.get('buildpacks', []))
     print("\n")
+    return all_apps
 
+
+def _fetch_env(all_apps: list[App]) -> list[App]:
     print("Fetching environment variables...")
     for index, app in enumerate(all_apps):
         print(f"Fetching env {index + 1}/{len(all_apps)}", end="\r")
@@ -87,17 +127,8 @@ def main():
         parsed_env_response = json.loads(env_response_raw) # TODO: 404 case
         app.env = parsed_env_response
     print("\n")
+    return all_apps
 
-
-    print("Apps:")
-    pprint(all_apps)
-
-def _construct_lifecycle(app: dict) -> AppLifecycle:
-    lifecycle = app.get("lifecycle", {})
-    return AppLifecycle(
-            type=lifecycle.get("type"),
-            buildpacks=lifecycle.get("data", {}).get("buildpacks") # TODO: Docker apps
-            )
 
 if __name__ == "__main__":
     main()
